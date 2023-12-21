0cd7a7c4b7e79600b4d8fa62619307807653e2fd
ZackaryW <36378555+ZackaryW@users.noreply.github.com>
2023-12-21 09:33:38 -0800

    feat: new singleton metaclass type

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ for folder in os.listdir("zrcl3"):
 
 setup(
     name="zrcl3",
-    version="1.0.5",
+    version="1.0.6",
     packages=folders + ["zrcl3"],
     description="zack's common reusable library",
     author="ZackaryW",
